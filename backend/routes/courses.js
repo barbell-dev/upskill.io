ae614f008f8e51ffd2fd8e5186e6406fa66cb542
Varun Dunna <varun.dunna2524@gmail.com>
2024-10-04 22:24:23 +0530

    update

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const jwt = require("jsonwebtoken");
+let log = console.log;
 const dotenv = require("dotenv");
 dotenv.config();
 // const {body-parser} =
 const userAuth = require("../middlewares/userAuth");
-const { PurchasesModel } = require("../db/db");
+const { PurchasesModel, CoursesModel } = require("../db/db");
 const coursesRouter = Router();
 // const app = express();
 // coursesRouter.use
@@ -21,17 +22,45 @@ coursesRouter.post("/purchaseCourse", async (req, res) => {
     courseName: courseName,
   })
     .then(() => {
-      res.json({ message: `Course ${courseName} inserted.` });
+      res.json({ message: `Course ${courseName} inserted.`, status: 200 });
       return;
     })
     .catch((e) => {
-      res.json({ message: `Unknown error occured` });
+      res.json({ message: `Unknown error occured`, status: 503 });
       console.log(e);
 
       return;
     });
 });
-coursesRouter.get("/viewPurchasedCourses", (req, res) => {});
-coursesRouter.get("/viewAllCourses", (req, res) => {});
+coursesRouter.get("/viewPurchasedCourses", async (req, res) => {
+  const token = req.headers.token;
+  const userData = jwt.verify(token, process.env.JWT_SECRET);
+  const userId = userData._id;
+  try {
+    const purchasedCoursesOfLoggedInUser = await PurchasesModel.find({
+      purchasedBy: userId,
+    });
+    res.json({
+      message: "Fetched the courses of logged in user successfully.",
+      status: 200,
+      purchasedCourses: purchasedCoursesOfLoggedInUser,
+    });
+    return;
+  } catch (e) {
+    res.json({ message: "Unknown error occured.", status: 503 });
+    log(e);
+    return;
+  }
+});
+coursesRouter.get("/viewAllCourses", async (req, res) => {
+  const allCourses = await CoursesModel.find().then(() => {
+    res.json({
+      message: "Fetched all courses successfully.",
+      status: 200,
+      allCourses: allCourses,
+    });
+    return;
+  });
+});
 coursesRouter.get("/viewParticularCourse", (req, res) => {});
 module.exports = coursesRouter;
